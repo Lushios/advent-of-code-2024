ac94da5ac0bfeb5c2b2bd0398999e99e8ea19b14
Alex Hendin <Alex.Hendin@plexusworldwide.com>
2024-12-07 12:42:05 +0200

    day 7 very hehe

---
+++ 7/part2.py
@@ -0,0 +1,35 @@
+from operator import add, mul
+from itertools import product
+
+
+with open("input.txt") as file:
+    data = file.read().splitlines()
+
+
+def a_concat_of_my_very_own(a, b):
+    return int(str(a) + str(b))
+
+
+operators = [add, mul, a_concat_of_my_very_own]
+
+
+def check_result(result, numbers):
+    operator_combinations = product(operators, repeat=len(numbers) - 1)
+    for combination in operator_combinations:
+        current_sum = numbers[0]
+        for i in range(len(combination)):
+            current_sum = combination[i](current_sum, numbers[i + 1])
+        if current_sum == result:
+            return True
+    return False
+
+
+answer = 0
+for line in data:
+    result, numbers = line.split(': ')
+    result = int(result)
+    numbers = [int(x) for x in numbers.split()]
+    if check_result(result, numbers):
+        answer += result
+
+print(answer)
