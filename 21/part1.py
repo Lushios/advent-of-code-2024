31068f30327d13c9aebe0b28456b7e10b54fcb78
Alex Hendin <Alex.Hendin@plexusworldwide.com>
2024-12-26 13:17:15 +0200

    3 place coulda been worse

--- 21/part1.py
+++ 21/part1.py
@@ -30,17 +30,29 @@ def numpad_into_arrows(code):
     for button in code:
         button_x, button_y = numpad_coordinates[button]
         dx, dy = button_x - x, button_y - y
+        if dx < 0:
+            if button_x == 0 and y == 3:
+                arrows.extend(['^' for _ in range(-dy)])
+                arrows.extend(['<' for _ in range(-dx)])
+                arrows.append('A')
+                x, y = button_x, button_y
+                continue
+            arrows.extend(['<' for _ in range(-dx)])
+        if dy > 0:
+            if button_y == 3 and x == 0:
+                arrows.extend(['>' for _ in range(dx)])
+                arrows.extend(['v' for _ in range(dy)])
+                arrows.append('A')
+                x, y = button_x, button_y
+                continue
+            arrows.extend(['v' for _ in range(dy)])
         if dy < 0:
             arrows.extend(['^' for _ in range(-dy)])
         if dx > 0:
             arrows.extend(['>' for _ in range(dx)])
-        if dy > 0:
-            arrows.extend(['v' for _ in range(dy)])
-        if dx < 0:
-            arrows.extend(['<' for _ in range(-dx)])
         arrows.append('A')
         x, y = button_x, button_y
-    print(''.join(arrows))
+    # print(''.join(arrows))
     return arrows
 
 
@@ -50,24 +62,32 @@ def arrows_into_arrows(code):
     for arrow in code:
         arrow_x, arrow_y = arrows_coordinates[arrow]
         dx, dy = arrow_x - x, arrow_y - y
-        if dy < 0:
-            arrows.extend(['^' for _ in range(-dy)])
-        if dx > 0:
-            arrows.extend(['>' for _ in range(dx)])
+        if dx < 0:
+            if arrow_x == 0 and y == 0:
+                arrows.extend(['v' for _ in range(dy)])
+                arrows.extend(['<' for _ in range(-dx)])
+                arrows.append('A')
+                x, y = arrow_x, arrow_y
+                continue
+            arrows.extend(['<' for _ in range(-dx)])
         if dy > 0:
             arrows.extend(['v' for _ in range(dy)])
-        if dx < 0:
-            arrows.extend(['<' for _ in range(-dx)])
+        if dx > 0:
+            arrows.extend(['>' for _ in range(dx)])
+        if dy < 0:
+            arrows.extend(['^' for _ in range(-dy)])
         arrows.append('A')
         x, y = arrow_x, arrow_y
-    print(''.join(arrows))
+    # print(''.join(arrows))
     return arrows
 
 
 result = 0
 for code in data:
     final_input = ''.join(arrows_into_arrows(arrows_into_arrows(numpad_into_arrows(code))))
+    # print(arrows_into_arrows(arrows_into_arrows(['^', '>'])))
+    # print(len(arrows_into_arrows(arrows_into_arrows(['^', '>']))))
+
     print(len(final_input), int(code[:3]))
-    # print(final_input)
     result += len(final_input) * int(code[:3])
 print(result)
--- 21/part2.py
+++ 21/part2.py
@@ -1,13 +1,81 @@
 with open("input.txt") as file:
     data = file.read().splitlines()
 
-lines = [row.split() for row in data]
+numpad_coordinates = {
+    '7': (0, 0),
+    '8': (1, 0),
+    '9': (2, 0),
+    '4': (0, 1),
+    '5': (1, 1),
+    '6': (2, 1),
+    '1': (0, 2),
+    '2': (1, 2),
+    '3': (2, 2),
+    '0': (1, 3),
+    'A': (2, 3),
+}
 
-set1 = [int(line[0]) for line in lines]
-set2 = [int(line[1]) for line in lines]
+arrows_coordinates = {
+    '^': (1, 0),
+    'A': (2, 0),
+    '<': (0, 1),
+    'v': (1, 1),
+    '>': (2, 1),
+}
+
+
+def numpad_into_arrows(code):
+    x, y = numpad_coordinates['A']
+    arrows = []
+    for button in code:
+        button_x, button_y = numpad_coordinates[button]
+        dx, dy = button_x - x, button_y - y
+        if dx < 0:
+            if button_x == 0 and y == 3:
+                arrows.append('^')
+                dy += 1
+            arrows.extend(['<' for _ in range(-dx)])
+        if dy > 0:
+            arrows.extend(['v' for _ in range(dy)])
+            if button_y == 3 and x == 0:
+                arrows.append('>')
+                dx -= 1
+        if dy < 0:
+            arrows.extend(['^' for _ in range(-dy)])
+        if dx > 0:
+            arrows.extend(['>' for _ in range(dx)])
+        arrows.append('A')
+        x, y = button_x, button_y
+    print(''.join(arrows))
+    return arrows
+
+
+def arrows_into_arrows(code):
+    x, y = arrows_coordinates['A']
+    arrows = []
+    for arrow in code:
+        arrow_x, arrow_y = arrows_coordinates[arrow]
+        dx, dy = arrow_x - x, arrow_y - y
+        if dx < 0:
+            arrows.extend(['<' for _ in range(-dx)])
+        if dy > 0:
+            arrows.extend(['v' for _ in range(dy)])
+        if dy < 0:
+            arrows.extend(['^' for _ in range(-dy)])
+        if dx > 0:
+            arrows.extend(['>' for _ in range(dx)])
+        arrows.append('A')
+        x, y = arrow_x, arrow_y
+    # print(''.join(arrows))
+    return arrows
+
 
 result = 0
-for i in range(len(set1)):
-    result += set1[i] * len([1 for element in set2 if element == set1[i]])
+for code in data:
+    final_input = ''.join(arrows_into_arrows(arrows_into_arrows(numpad_into_arrows(code))))
+    # print(arrows_into_arrows(arrows_into_arrows(['^', '>'])))
+    # print(len(arrows_into_arrows(arrows_into_arrows(['^', '>']))))
 
+    print(len(final_input), int(code[:3]))
+    result += len(final_input) * int(code[:3])
 print(result)
